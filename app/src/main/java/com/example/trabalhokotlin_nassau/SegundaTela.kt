98edcee1c636d010e4b4bf04c2f7095c70914776
Tiago Tenório <tiagotenoriogitcommits@gmail.com>
2023-04-14 00:11:22 -0300

    SEGUNDO COMMIT COM NOVO DESIGN, ENTRETANTO SEM CONSEGUIR ENVIAR TROCAR DE TELA DEVIDO A UM MILHÃO DE ERROS.

--- app/src/main/java/com/example/trabalhokotlin_nassau/SegundaTela.kt
+++ app/src/main/java/com/example/trabalhokotlin_nassau/SegundaTela.kt
@@ -1,16 +1,11 @@
 package com.example.trabalhokotlin_nassau
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import android.widget.Button
 
-
-class MainActivity : AppCompatActivity() {
+class SegundaTela : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_segunda_tela)
     }
-
 }
